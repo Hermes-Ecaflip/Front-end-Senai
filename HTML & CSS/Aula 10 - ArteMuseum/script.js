30b744d211535f56df75dfa37adecf961d026e55
Hermes-Ecaflip <angelocks22@gmail.com>
2024-04-25 17:22:01 -0300

    update

--- HTML & CSS/Aula 10 - ArteMuseum/script.js
+++ HTML & CSS/Aula 10 - ArteMuseum/script.js
@@ -1,12 +1,8 @@
-function btnMobillee() {
-    var menuMobile = document.getElementById('menumobile');
-    var menuDesktop = document.getElementById('menudesktop');
-
-    if (menuMobile.style.display === 'block') {
-        menuMobile.style.display = 'none';
-        menuDesktop.style.display = 'block';
+function btnMobile() {
+    var menuMobile = document.getElementById("menumobile");
+    if (menuMobile.style.display === "block") {
+        menuMobile.style.display = "none";
     } else {
-        menuMobile.style.display = 'block';
-        menuDesktop.style.display = 'none';
+        menuMobile.style.display = "block";
     }
 }
