9094832e7759cc4f7fb9331317dfbbaf9b3c9fb4
Ângelo Passos <angelocks22@gmail.com>
2024-05-02 23:07:00 -0300

    Update

--- HTML & CSS/Prova - 30 de maio 2024/script.js
+++ HTML & CSS/Prova - 30 de maio 2024/script.js
@@ -1,3 +1,28 @@
+// Função para alternar a barra lateral
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+// Índice inicial do slide
+var slideIndex = 1;
+mostrarSlides(slideIndex);
+
+// Função para mover para o próximo ou anterior slide
+function moverSlide(n) {
+  mostrarSlides(slideIndex += n);
+}
+
+// Função para mostrar o slide atual
+function mostrarSlides(n) {
+    var slides = document.getElementsByClassName("carrossel-slide");
+    if (n > slides.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = slides.length;}
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slides[slideIndex-1].classList.add("active");
+}
+
+
+
+
