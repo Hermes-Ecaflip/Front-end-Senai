3ab7b76f858fbe519fd422dde614f9979a250dce
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-27 16:35:30 -0300

    Update construtor.js

--- Javascript/Aula - 06 Objetos/scripts/construtor.js
+++ Javascript/Aula - 06 Objetos/scripts/construtor.js
@@ -7,10 +7,24 @@ class ClienteVip {
         this.pagamentos = PagamentosP;
     }
         sacar(valor){
-
+            if(this.saldo < valor){
+                return `operação não autorizada!`
+            }else{
+                this.saldo -= valor
+                return `O saque' do valor R$ ${valor} foi efetuado
+                com sucesso!`
+            }
     }
     depositar(valor){
-
+        if(valor <= 0){
+            return `Operação não autorizada!`
+        }else{
+            this.saldo += valor
+            return `O deposito do valor R$ ${valor} foi efetudado com sucesso!`
+        }
+    }
+    verificarSaldo(){
+        return this.saldo
     }
 
 }
