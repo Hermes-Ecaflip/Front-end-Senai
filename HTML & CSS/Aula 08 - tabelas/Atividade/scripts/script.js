611395cb9315049bd9d07c21f60968356d762b2e
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-27 14:34:24 -0300

    Upadate  Tabela

--- HTML & CSS/Aula 08 - tabelas/Atividade/scripts/script.js
+++ HTML & CSS/Aula 08 - tabelas/Atividade/scripts/script.js
@@ -1,26 +1,36 @@
+// Aguarda o carregamento completo do DOM.
 document.addEventListener('DOMContentLoaded', function() {
+    // Impede que o formulário seja enviado e a página recarregada.
     document.querySelector('form').onsubmit = function(event) {
-        event.preventDefault(); // Impede que o formulário seja enviado e a página recarregada.
+        event.preventDefault();
         calcularSalarioFamilia();
+        // Exibe o modal com o resultado do cálculo.
+        var myModal = new bootstrap.Modal(document.getElementById('modalSalarioFamilia'));
+        myModal.show();
     };
 });
 
+// Função para calcular o salário família.
 function calcularSalarioFamilia() {
+    // Obtém os valores dos inputs e inicializa as variáveis.
     var salario = parseFloat(document.getElementById('Salario').value);
     var filhos = parseInt(document.getElementById('Filhos').value);
     var resultado = document.getElementById('resultado');
     var valorPorFilho;
     var totalSalarioFamilia;
 
+    // Define o valor por filho com base no salário.
     if (salario <= 806.80) {
-        valorPorFilho = 29.16; // Valor para salários até R$ 806,80.
+        valorPorFilho = 29.16; // Salários até R$ 806,80.
     } else if (salario > 806.80 && salario <= 1212.64) {
-        valorPorFilho = 41.37; // Valor para salários entre R$ 806,81 e R$ 1.212,64.
+        valorPorFilho = 41.37; // Salários entre R$ 806,81 e R$ 1.212,64.
     } else {
+        // Informa que não há salário família para essa faixa de salário.
         resultado.innerHTML = 'Não há Salário Família para essa faixa de salário.';
         return;
     }
 
+    // Calcula o total do salário família e exibe o resultado.
     totalSalarioFamilia = valorPorFilho * filhos;
-    resultado.innerHTML = 'O Salário Família total é: R$ ' + totalSalarioFamilia.toFixed(2);
+    resultado.innerHTML = totalSalarioFamilia.toFixed(2);
 }
