f5ba40e24923473419978f70d2a1935e1ee136ad
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-27 17:09:12 -0300

    Atividade

---
+++ Javascript/Aula - 06 Objetos/Atividade/scripts/script.js
@@ -0,0 +1,9 @@
+class Carro {
+    constructor(marca, modelo, ano, cor,velocidadeMaxima){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+}
