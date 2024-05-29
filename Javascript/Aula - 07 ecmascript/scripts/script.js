70aa0ee1173427647fd94727068157a95fad145c
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-29 14:43:36 -0300

    ECMASCRIPT

---
+++ Javascript/Aula - 07 ecmascript/scripts/script.js
@@ -0,0 +1,14 @@
+var texto = "Olá mundo, o mundo está bonito, tchau mundo";
+var nome = "Ângelo";
+var sobrenome = "Passos";
+
+console.log("-------REPLACE------")
+console.log(texto);
+// replace = replace troca o primeiro termo achado
+console.log(texto.replace("mundo","banana"));
+// replaceAll = o All troca todos os termos encontrados
+console.log(texto.replaceAll("mundo","olá"));
+
+console.log(texto.replaceAll("mundo","olá"));
+
+console.log("------IndexOf/Includes------")
