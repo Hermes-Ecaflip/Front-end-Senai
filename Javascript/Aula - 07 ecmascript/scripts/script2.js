f5b762192b13037b65d8d5a2db78b3f476be0d62
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-29 15:13:40 -0300

    Update script2.js

--- Javascript/Aula - 07 ecmascript/scripts/script2.js
+++ Javascript/Aula - 07 ecmascript/scripts/script2.js
@@ -1,4 +1,10 @@
 // promisses
 var promessa = new Promise((resolve,reject) => {
-    
+    let nome = "Ângelo";
+
+    if(nome == "Ângelo"){
+        resolve("O usuario foi encontrado!")
+    }else{
+        reject("O usuario não é o Ângelo")
+    }
 })
