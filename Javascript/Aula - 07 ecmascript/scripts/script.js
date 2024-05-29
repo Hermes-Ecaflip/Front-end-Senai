80b34c4f85d4691c579e088f6bb87aef41d0fadb
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-29 15:00:07 -0300

    Update script.js

--- Javascript/Aula - 07 ecmascript/scripts/script.js
+++ Javascript/Aula - 07 ecmascript/scripts/script.js
@@ -2,13 +2,26 @@ var texto = "Olá mundo, o mundo está bonito, tchau mundo";
 var nome = "Ângelo";
 var sobrenome = "Passos";
 
-console.log("-------REPLACE------")
+console.log("-------REPLACE------");
 console.log(texto);
 // replace = replace troca o primeiro termo achado
-console.log(texto.replace("mundo","banana"));
+console.log(texto.replace("mundo", "banana"));
 // replaceAll = o All troca todos os termos encontrados
-console.log(texto.replaceAll("mundo","olá"));
+console.log(texto.replaceAll("mundo", "olá"));
 
-console.log(texto.replaceAll("mundo","olá"));
+console.log("------IndexOf/Includes------");
+var alunos = ["Leo", "Juliana", "Fernanda", nome];
 
-console.log("------IndexOf/Includes------")
+// IndexOf = pesquisa se um termo existe e retorna onde ele está
+// se -1 é porque o termo não existe
+// se 0 ou maior é porque o termo existe e se localiza nesse índice
+var pesquisa = alunos.indexOf("Leandro");
+// Includes = pesquisa se um termo existe e retorna true ou false
+var pesquisa2 = alunos.includes("Alexandre");
+console.log(pesquisa);
+
+if (pesquisa >= 0) {
+    console.log("Item foi encontrado!");
+} else {
+    console.log("Item não encontrado! Tente novamente.");
+}
