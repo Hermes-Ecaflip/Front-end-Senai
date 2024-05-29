e2dea95dc12c3ceb31e89bfd9de4a6e57b3751bf
Hermes-Ecaflip <angelocks22@gmail.com>
2024-05-29 15:59:01 -0300

    Update script2.js

--- Javascript/Aula - 07 ecmascript/scripts/script2.js
+++ Javascript/Aula - 07 ecmascript/scripts/script2.js
@@ -1,14 +1,31 @@
-// promisses
-var promessa = new Promise((resolve,reject) => {
-    let nome = "Ângelo";
+// Definição de uma função que retorna uma promessa que se resolve após um tempo
+function codigoDemorado() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Enrolei");
+            resolve();
+        }, 3000);
+    });
+}
 
-    if(nome == "Ângelo"){
-        resolve("O usuario foi encontrado!")
-    }else{
-        reject("O usuario não é o Ângelo")
-    }
-})
+// Função assíncrona que aguarda a conclusão da promessa
+async function carregando() {
+    console.log("Comecei");
+    await codigoDemorado(); // Aguarda a promessa ser resolvida
+    console.log("Terminei");
+}
 
-promessa.then((resultado) => {
-    console.log(resultado)
-}) 
+// Chamada da função assíncrona
+carregando();
+
+// Exemplo de uso da promessa sem 'await', o que causa um comportamento não sincronizado
+console.log("Comecei");
+codigoDemorado(); // A promessa é iniciada, mas não é aguardada
+console.log("Terminei"); // Este log é executado imediatamente após o início da promessa, não após sua conclusão
+
+// Exemplo de tratamento de erro com 'catch'
+// promessa.then(faça => {algo esperado um erro})
+// catch = executa o código esperando todos os resultados não esperados (erros, rejects)
+promessa.catch((resultado) => {
+    console.log(resultado);
+});
