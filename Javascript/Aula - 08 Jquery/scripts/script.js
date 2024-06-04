8a9e995cb1437b144fd170bc3b590e12f35f6ed4
Hermes-Ecaflip <angelocks22@gmail.com>
2024-06-04 15:16:35 -0300

    Update script.js

--- Javascript/Aula - 08 Jquery/scripts/script.js
+++ Javascript/Aula - 08 Jquery/scripts/script.js
@@ -1,2 +1,7 @@
 var paragrafoPrincipalMetodoAntigo = document.querySelector("#paragrafoPrincipal")
 var pPrincipal = $("#paragrafoPrincipal").text("OLHA QUE TOP")
+
+var nomeUsuario = prompt("Qual é o seu nome?");
+
+$("#nomeUsuario").text(nomeUsuario)
+document.querySelector("#nomeUsuario").innerHTML = nomeUsuario
