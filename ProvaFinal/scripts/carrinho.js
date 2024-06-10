b846357b13bc2c2da51a9ca10567bd71b7d9e83f
Hermes-Ecaflip <angelocks22@gmail.com>
2024-06-10 17:49:27 -0300

    Update

--- ProvaFinal/scripts/carrinho.js
+++ ProvaFinal/scripts/carrinho.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-  // Recupera as poltronas selecionadas do localStorage
-  var poltronasSelecionadas = JSON.parse(localStorage.getItem('poltronasSelecionadas')) || [];
-  // Exibe as poltronas no carrinho
-  poltronasSelecionadas.forEach(function(poltrona) {
-    $('#carrinho .card-body').append('<p>Poltrona: ' + poltrona + '</p>');
-  });
+  // Recupera as informações do filme do localStorage
+  var filmeInfo = JSON.parse(localStorage.getItem('filmeInfo'));
 
-  // Suponha que você também tenha salvo as informações do filme no localStorage
-  var filmeInfo = JSON.parse(localStorage.getItem('filmeInfo')) || {};
-  if (filmeInfo.titulo && filmeInfo.imagem) {
+  if (filmeInfo) {
+    // Exibe a imagem e o título do filme no carrinho
     $('#carrinho .card-body').prepend(
-      '<img src="' + filmeInfo.imagem + '" class="card-img-top" alt="' + filmeInfo.titulo + '">' +
+      '<img src=https://image.tmdb.org/t/p/w342/rC5RqXtFoTkBhNWE1dczwi4dZrX.jpg" class="card-img-top" alt="' + filmeInfo.titulo + '">' +
       '<h5 class="card-title">' + filmeInfo.titulo + '</h5>'
     );
+
+    // Exibe as poltronas selecionadas no carrinho
+    filmeInfo.poltronas.forEach(function(poltrona) {
+      $('#carrinho .card-body').append('<p>Poltrona: ' + poltrona + '</p>');
+    });
+  } else {
+    // Se não houver informações, exibe uma mensagem ou redireciona
+    $('#carrinho .card-body').append('<p>Nenhum filme ou poltrona selecionada.</p>');
   }
 });
