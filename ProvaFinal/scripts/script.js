f34f53ae890af342359b8e305b923e6489f43a89
Hermes-Ecaflip <angelocks22@gmail.com>
2024-06-10 15:18:53 -0300

    Update

--- ProvaFinal/scripts/script.js
+++ ProvaFinal/scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
   // Detecta a sequência de teclas do Código Konami
   $(document).keydown(function(e) {
-    inputSequence += e.keyCode;
+    inputSequence += e.keyCode.toString();
     if (inputSequence === konamiCode) {
       executeLogoSpin();
     }
@@ -48,4 +48,52 @@ $(document).ready(function() {
     $('body').addClass('light-theme').removeClass('dark-theme');
   }
   updateThemeIcons(); // Atualiza os ícones ao carregar a página
+});
+
+$(document).ready(function() {
+  // Adiciona evento de clique para cada filme
+  $('#filmes .card a, #filmes2 .card a').click(function(event) {
+    event.preventDefault(); // Previne a ação padrão do link
+    $('#modalSelecaoAssentos').modal('show'); // Abre o modal
+  });
+});
+
+$(document).ready(function() {
+  // Função para gerar poltronas
+  function gerarPoltronas() {
+    const assentosCinema = $('#assentosCinema');
+    assentosCinema.empty(); // Limpa os assentos existentes
+    let total = 0; // Inicializa o total
+
+    for (let i = 0; i < 3; i++) { // 3 fileiras
+      const fileira = $('<div>').addClass('d-flex justify-content-center mb-2');
+      for (let j = 0; j < 6; j++) { // 6 poltronas por fileira
+        const poltrona = $('<button>').addClass('btn border me-1 poltrona');
+        // Adiciona o ícone e o número em linhas separadas
+        poltrona.html(`<i class="fa-solid fa-couch fa-xl"></i><span class="poltrona-numero">${i * 6 + j + 1}</span>`);
+        poltrona.on('click', function() {
+          $(this).toggleClass('btn-success'); // Muda a cor para verde
+          const valorPoltrona = 29.90;
+          if ($(this).hasClass('btn-success')) {
+            total += valorPoltrona; // Adiciona o valor ao total
+          } else {
+            total -= valorPoltrona; // Subtrai o valor do total
+          }
+          $('#total').text(`Total: R$${total.toFixed(2)}`); // Atualiza o total
+        });
+        fileira.append(poltrona);
+      }
+      assentosCinema.append(fileira);
+    }
+  }
+
+  // Inicializa o modal com as poltronas
+  $('#modalSelecaoAssentos').on('show.bs.modal', function() {
+    gerarPoltronas(); // Gera as poltronas
+  });
+
+  // Limpa o total ao fechar o modal
+  $('#modalSelecaoAssentos').on('hidden.bs.modal', function() {
+    $('#total').text('Total: R$0.00');
+  });
 });
